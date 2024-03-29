d65d386220c8fecf4bf0513042faacb55b3bc5f8
Diego Vazquez Sanchez <diegov@Diegos-MacBook-Pro.local>
2024-03-28 18:40:04 -0600

    Starting Project

---
+++ iSync/iSync/iSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  iSyncApp.swift
+//  iSync
+//
+//  Created by Diego Vazquez Sanchez on 28/03/24.
+//
+
+import SwiftUI
+
+@main
+struct iSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
