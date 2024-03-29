164e5a70e4116f4a5b87d6346c7855928e96824f
Diego Vazquez Sanchez <diegov@Diegos-MacBook-Pro.local>
2024-03-28 18:48:47 -0600

    126.DataModel

---
+++ iSync/iSync/Model/PostModel.swift
@@ -0,0 +1,12 @@
+//
+//  PostModel.swift
+//  iSync
+//
+//  Created by Diego Vazquez Sanchez on 28/03/24.
+//
+
+import Foundation
+
+struct PostModel : Decodable {
+    var token : String
+}
---
+++ iSync/iSync/ViewModel/PostViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  PostViewModel.swift
+//  iSync
+//
+//  Created by Diego Vazquez Sanchez on 28/03/24.
+//
+
+import Foundation
+
+class PostViewModel : ObservableObject {
+    
+    @Published var authenticated = 0
+    
+    func login(email: String, password: String){
+        
+    }
+}
